f78ef9fd0b813bd37aab2b1fdd7c98271293a7f3
shayan <sak704@nyu.edu>
2022-02-07 16:49:28 -0800

    commit tree f8bcc8602352b8543b6ac3551d268fd00db02b2a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function saturdayFun(activity = `roller-skate`) {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective(flair = `*`) {
+    const part1 = `You are`;
+
+    return function qualityPerson(adjective = `specials`) {
+        return `${part1} ${flair}${adjective}${flair}!`;
+    }
+
+}
